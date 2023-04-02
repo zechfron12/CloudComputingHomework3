1b93b6a6eae321297f223fcf079a94205ad02e60
Radu Chelaru <raduchelaru1@gmail.com>
2023-04-02 20:36:34 +0300

    directory change

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+import { EventColor } from 'calendar-utils';
+
+export const environment = {
+  production: true,
+};
+
+export const colors: Record<string, EventColor> = {
+  yellow: {
+    primary: '#ffd740',
+    secondary: '#FDF1BA',
+  },
+  purple: {
+    primary: '#673ab7',
+    secondary: '#FAE3E3',
+  },
+};
